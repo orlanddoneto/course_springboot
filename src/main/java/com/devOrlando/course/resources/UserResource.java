8d0e99373dc9dd21528a82827142f840ac65a55c
orlanddoneto <neto020735@gmail.com>
2023-10-11 14:01:14 -0300

    User entity and resource

---
+++ src/main/java/com/devOrlando/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.devOrlando.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devOrlando.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User("name",90L,"email@email.com","999999","senha");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
