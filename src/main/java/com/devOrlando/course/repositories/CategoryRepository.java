f968fff908d011cf7b2eadc48efff666f29f354d
orlanddoneto <neto020735@gmail.com>
2023-10-27 11:42:58 -0300

    implementation category

---
+++ src/main/java/com/devOrlando/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.devOrlando.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devOrlando.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
